79d5d4f8f0838d11fafcd23a6cccf73e52744c1b
José María <josemmazzei@gmail.com>
2020-11-06 11:31:05 -0300

    Define category and post models

--- models/category.js
+++ models/category.js
@@ -1,4 +1,19 @@
 module.exports = (sequelize, type) => {
-  const categoryModel = sequelize.define('category', {})
+  const categoryModel = sequelize.define(
+    'category',
+    {
+      id: {
+        type: type.INTEGER,
+        primaryKey: true,
+      },
+      categoria:{
+        type: type.STRING(120),
+        allowNull: false,
+      },
+    },
+    { timestamps: false,
+      tableName: 'categories'
+    },
+  )
   return categoryModel
 }
--- models/post.js
+++ models/post.js
@@ -1,8 +1,30 @@
 module.exports = (sequelize, type) => {
   const postModel = sequelize.define(
     'post',
-    {},
-
+    {
+      id: {
+        type: type.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      titulo: {
+        type: type.STRING(120),
+        allowNull: false,
+      },
+      contenido: {
+        type: type.TEXT,
+        allowNull: false,
+      },
+      imagen: {
+        type: type.STRING,
+        allowNull: false,
+      },
+      fecha_creacion: {
+        type: type.DATEONLY,
+        defaultValue: sequelize.NOW,
+        allowNull: false,
+      },
+    },
     { timestamps: false, tableName: 'posts' },
   )
   return postModel
